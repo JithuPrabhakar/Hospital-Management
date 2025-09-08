a816158e72aa56b219ece0e1c0fa5dd4acbf5df2
JithuPrabhakar <join.jitz@gmail.com>
2025-09-08 14:40:46 +0530

    creates pages for frontend

---
+++ client/src/pages/DoctorPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../components/common/Header";
+
+const DoctorPage = () => {
+  return (
+    <>
+      <Header />
+      <h2>DoctorPage</h2>
+    </>
+  );
+};
+
+export default DoctorPage;
