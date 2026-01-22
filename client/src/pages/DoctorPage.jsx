e26bce2c7136701be01beaf2d01ad1bf7d31d9f2
JithuPrabhakar <join.jitz@gmail.com>
2026-01-22 11:10:48 +0530

    small changes

--- client/src/pages/DoctorPage.jsx
+++ client/src/pages/DoctorPage.jsx
@@ -1,6 +1,7 @@
-import React, { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import Header from "../components/common/Header";
+import { useState, useEffect, useContext } from 'react';
+import axios from 'axios';
+import AuthContext from '../context/AuthContext';
+import Navbar from '../components/Navbar';
 import "./DoctorPage.css";
 
 const DoctorPage = () => {
